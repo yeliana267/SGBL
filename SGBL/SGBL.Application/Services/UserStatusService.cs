63f9b1871982601cf6066a44c5fcd5042442963d
Rafael-Jr34 <rafaeljuniorsilfagomez@gmail.com>
2025-10-24 21:20:39 -0400

    Re-creación de la clase UserStatusService, que se elimino por accidente en el push anterior

---
+++ SGBL/SGBL.Application/Services/UserStatusService.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using SGBL.Application.Dtos.User;
+using SGBL.Application.Interfaces;
+using SGBL.Domain.Entities;
+using SGBL.Domain.Interfaces;
+
+
+namespace SGBL.Application.Services
+{
+    public class UserStatusService : GenericService<UserStatus, UserStatusDto>, IUserStatusService
+    {
+        private readonly IUserStatusRepository _userStatusRepository;
+        private readonly IMapper _mapper;
+        private readonly IServiceLogs _serviceLogs;
+        public UserStatusService(IUserStatusRepository userStatusRepository, IMapper mapper, IServiceLogs serviceLogs) : base(userStatusRepository, mapper, serviceLogs)
+        {
+            _userStatusRepository = userStatusRepository;
+            _mapper = mapper;
+            _serviceLogs = serviceLogs;
+        }
+    }
+}
